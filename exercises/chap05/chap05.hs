7bf08fd71327b10913c9589a087b0fc75c9620dc
GENKI FURUKAWA <genki.furukawa@gmail.com>
2025-03-16 15:18:56 +0900

    [wip] chap5を追加

---
+++ exercises/chap05/chap05.hs
@@ -0,0 +1,13 @@
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+-- 型aと型bを引数、型cを返す関数を第一引数、型aのリストと型bのリストを引数にして、型cのリストを返す関数
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+  where
+    g x y = f y x
