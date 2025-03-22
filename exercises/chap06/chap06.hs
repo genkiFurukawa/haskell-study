d61250085119de122a416cd2489c2d9c4e032dfa
GENKI FURUKAWA <genki.furukawa@gmail.com>
2025-03-22 16:53:54 +0900

    [wip] chap06

--- exercises/chap06/chap06.hs
+++ exercises/chap06/chap06.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List (find)
 
 -- 文字の数値をとってoffsetを足したものを文字に変換
 encode :: Int -> String -> String
@@ -6,3 +7,10 @@ encode offset msg = map (\c -> chr $ ord c + offset) msg
 
 decode :: Int -> String -> String
 decode shift msg = encode (negate shift) msg
+
+-- 数値の格桁の値の合計を返す関数
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1 ..]
--- notes/chap06/note.md
+++ notes/chap06/note.md
@@ -8,3 +8,14 @@ ghci> group $ sort ["boom", "bip", "bip", "boom", "boom"]
 ghci> (group . sort) ["boom", "bip", "bip", "boom", "boom"]
 [["bip","bip"],["boom","boom","boom"]]
 ```
+
+- find は(a -> Bool)と t a 型を受け取っって、Maybe a 型を返す関数.t は Foldable を満たす（リストなど）。
+
+```
+ghci> :t find
+find :: Foldable t => (a -> Bool) -> t a -> Maybe a
+ghci> :info Maybe
+type Maybe :: * -> *
+data Maybe a = Nothing | Just a
+ghci> find (> 4) [3,4,56]
+```
